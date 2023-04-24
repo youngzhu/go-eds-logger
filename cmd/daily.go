f0e77307f0ae2744988c4c97884577796da9865f
young-win <young.zhu.2012@gmail.com>
2023-04-24 18:17:37 +0800

    按天填日志，支持负数，即今天之前的起点

--- cmd/daily.go
+++ cmd/daily.go
@@ -10,38 +10,43 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/youngzhu/godate"
 	"goeds/logger"
-	"strconv"
+	"log"
+)
+
+var (
+	logStart int
+	logDays  int
 )
 
 // dailyCmd represents the daily command
-// 参数不能支持负数？？
+// goeds log d -s=-1 -d=1
 var dailyCmd = &cobra.Command{
 	Use:     "daily(d) m n",
 	Aliases: []string{"d"},
 	Short:   "按天填写日志",
-	Args:    cobra.ExactArgs(2),
+	//Args:    cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// 获取参数
-		from, err := strconv.Atoi(args[0])
-		if err != nil {
-			return err
-		}
-		days, err := strconv.Atoi(args[1])
-		if err != nil {
-			return err
-		}
+		//from, err := strconv.Atoi(args[0])
+		//if err != nil {
+		//	return err
+		//}
+		//days, err := strconv.Atoi(args[1])
+		//if err != nil {
+		//	return err
+		//}
 
-		return dailyAction(from, days)
+		return dailyAction()
 	},
 }
 
-func dailyAction(from, days int) error {
-	//log.Println(from + 1)
-	//log.Println(days + 1)
+func dailyAction() error {
+	log.Println(logStart + 1)
+	log.Println(logDays + 1)
 
 	var logDay godate.Date
-	for i := 0; i < days; i++ {
-		diff := from + i
+	for i := 0; i < logDays; i++ {
+		diff := logStart + i
 		if diff >= 0 {
 			logDay, _ = godate.Today().AddDay(diff)
 		} else {
@@ -66,4 +71,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// dailyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	dailyCmd.Flags().IntVarP(&logStart, "start", "s", 0, "日志开始的起点。0：今天；-1：昨天；1：明天")
+	dailyCmd.Flags().IntVarP(&logDays, "days", "d", 0, "连续填写日志的天数")
+
 }
