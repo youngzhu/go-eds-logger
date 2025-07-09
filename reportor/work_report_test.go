1275cac5dca6e74b7b1c7fdfe23fa3976363d5e1
young-win <young.zhu.2012@gmail.com>
2025-07-09 14:33:29 +0800

    单元测试换个地方

--- reportor/work_report_test.go
+++ reportor/work_report_test.go
@@ -1,10 +1,6 @@
 package reportor
 
 import (
-	"fmt"
-	"github.com/spf13/viper"
-	"github.com/youngzhu/godate"
-	"log"
 	"testing"
 )
 
@@ -26,30 +22,4 @@ func Test_retrieveWorkReportFromLocal(t *testing.T) {
 	if workReport.LastWeekWorkContent == "" {
 		t.Error("LastWeekWorkContent should not empty.")
 	}
-}
-
-func TestWorkReport_formatDailyReportUrl(t *testing.T) {
-	reportUrl := fmt.Sprintf("%s&LogDate=%s", viper.GetString("urls.daily"), "2023-10-01")
-
-	want := "http://eds.newtouch.cn/eds3/worklog.aspx?tabid=0&LogDate=2023-10-01"
-	if reportUrl != want {
-		t.Errorf("Expected URL to be '%s', got '%s'", want, reportUrl)
-	}
-}
-
-func TestWorkReport_getHiddenParams(t *testing.T) {
-	// 先登录
-	err := login()
-	if err != nil {
-		t.Error("登录失败：", err)
-	}
-
-	reportUrl := fmt.Sprintf("%s&LogDate=%s", viper.GetString("urls.daily"), godate.Today())
-	log.Println(reportUrl)
-
-	_, err = getHiddenParams(reportUrl)
-	if err != nil {
-		t.Error("getHiddenParams error:", err)
-	}
-
 }
--- reportor/work_reportor_test.go
+++ reportor/work_reportor_test.go
@@ -1,10 +1,42 @@
 package reportor
 
-import "testing"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"github.com/youngzhu/godate"
+	"log"
+	"testing"
+)
 
 func Test_login(t *testing.T) {
 	err := login()
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func Test_formatDailyReportUrl(t *testing.T) {
+	reportUrl := fmt.Sprintf("%s&LogDate=%s", viper.GetString("urls.daily"), "2023-10-01")
+
+	want := "http://eds.newtouch.cn/eds3/worklog.aspx?tabid=0&LogDate=2023-10-01"
+	if reportUrl != want {
+		t.Errorf("Expected URL to be '%s', got '%s'", want, reportUrl)
+	}
+}
+
+func Test_getHiddenParams(t *testing.T) {
+	// 先登录
+	err := login()
+	if err != nil {
+		t.Error("登录失败：", err)
+	}
+
+	reportUrl := fmt.Sprintf("%s&LogDate=%s", viper.GetString("urls.daily"), godate.Today())
+	log.Println(reportUrl)
+
+	_, err = getHiddenParams(reportUrl)
+	if err != nil {
+		t.Error("getHiddenParams error:", err)
+	}
+
 }
